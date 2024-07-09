143352e0470d0c289ea6cecc3341fb6cb1fd2307
Мария Бадулина <volshica1988@mail.ru>
2024-07-09 15:38:52 +0500

    Функции возвращаются

--- js/functions.js
+++ js/functions.js
@@ -30,3 +30,57 @@ console.log('тест 2', stringPalindrome('ДовОд')); // true
 console.log('тест 3', stringPalindrome('Кекс')); // false
 // Это палиндром
 console.log('тест 4', stringPalindrome('Лёша на полке клопа нашёл ')); // true
+
+
+//startWork - начало рабочего дня
+//endWork - конец рабочего дня
+//startMeeting - начало встречи
+//durationMeeting - продолжительность встречи
+/*
+имяФункции('08:00', '17:30', '14:00', 90); // true
+имяФункции('8:0', '10:0', '8:0', 120);     // true
+имяФункции('08:00', '14:30', '14:00', 90); // false
+имяФункции('14:00', '17:30', '08:0', 90);  // false
+имяФункции('8:00', '17:30', '08:00', 900); // false
+*/
+
+// переводим время в минуты
+const timeInMinutes = (time) => {
+  const [hours, minutes] = time.split(':').map((num) => parseInt(num, 10));
+  return hours * 60 + minutes;
+};
+
+const workEndMeeting = (startWork, endWork, startMeeting, durationMeeting) => {
+  const startHoursMinutes = startWork.split(':');
+  const endHoursMinutes = endWork.split(':');
+  const removeZero = (number) => parseInt(number.toString().replace(/0+/g, ''), 10);
+  const hourInMinutesWork = (removeZero((endHoursMinutes[0] - startHoursMinutes[0])) * 60);
+  const minutesWork = parseInt(endHoursMinutes[1], 10) - parseInt(startHoursMinutes[1],10);
+  const lengthWork = hourInMinutesWork + minutesWork; //Длина рабочего дня
+
+  const hoursMinutesMeeting = startMeeting.split(':');
+  const hoursInMinutesMeeting = 0;
+  const minutesMeeting = parseInt(hoursMinutesMeeting[1], 10);
+  const timeMeeting = hoursInMinutesMeeting + minutesMeeting + durationMeeting; // продолжительность встречи
+
+  const workEndMeetingLength = timeInMinutes(endWork) - timeInMinutes(startMeeting); // выходит ли встреча за рабочее время
+
+  if(startHoursMinutes[0] > hoursMinutesMeeting[0]){
+    return false;
+  } else if (workEndMeetingLength < durationMeeting) {
+    return false;
+  }else if (lengthWork >= timeMeeting) {
+    return true;
+  } else if(lengthWork < timeMeeting) {
+    return false;
+  }
+
+};
+
+
+console.log(workEndMeeting('08:00', '17:30', '14:00', 90)); // true
+console.log(workEndMeeting('8:0', '10:0', '8:0', 120)); // true
+console.log(workEndMeeting('08:00', '14:50', '14:00', 90)); // false
+console.log(workEndMeeting('14:00', '17:30', '08:0', 90)); // false
+console.log(workEndMeeting('8:00', '17:30', '08:00', 900)); //false
+
